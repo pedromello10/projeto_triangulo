a23d298f010c50508feab605761dbf4bddbf3d6a
lszme <lszme@10.146.193.92>
2026-03-17 21:16:10 -0300

    fix: coentario de linhas

--- src/main/java/principal.java
+++ src/main/java/principal.java
@@ -4,10 +4,14 @@ import javax.swing.JOptionPane;
 public class principal {
 
     public static void main(String[] args) {
+        //variáveis
         double base, altura, area;
+        //entrada
         base = Double.parseDouble(JOptionPane.showInputDialog("Digite a base: "));
         altura = Double.parseDouble(JOptionPane.showInputDialog("Digite a altura: "));
+        //processamento
         area = (base * altura / 2.0);
+        //saida
         JOptionPane.showMessageDialog(null, "A área do triângulo é: " + area);
     }
 }
